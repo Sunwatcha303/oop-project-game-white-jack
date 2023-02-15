4bd8342faf2a2bf0da7fd1af6bcf04587d4ca1ea
Sunwatcha303 <watcharakorn301146@gmail.com>
2023-02-15 20:25:18 +0700

    init

---
+++ Game.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game {
+
+    public Game() {
+        initVariable();
+        initWindow();
+    }
+
+    public void initWindow(){
+        
+    }
+
+    public void initVariable(){
+        
+    }
+}
